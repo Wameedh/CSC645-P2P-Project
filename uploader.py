dd36a70154178e7bd2ab67aeb9818b6fd86cc3cf
Nathalia Sainez <nathaliasainez@sfs-wifi-dhcp-10-143-201-114.wifi.sfsu.edu>
2020-12-14 13:42:35 -0800

    Added sudo code for the uploader class

--- uploader.py
+++ uploader.py
@@ -3,7 +3,6 @@ from pwp import PWP
 from file_manager import FileManager
 from config import Config
 
-
 class Uploader:
 
     def __init__(self, peer_id, server, peer_uploader, address, torrent):
@@ -31,3 +30,39 @@ class Uploader:
         serialized_data = self.peer_uploader.recv(max_alloc_mem)
         data = pickle.loads(serialized_data)
         return data
+
+# When the client from P2 connects to P1, the server will create and thread the
+# uploader class (like the clienthandler class)
+
+# Uploader will communicate with the client of P2 directly, send request/response to
+# each other (like the Menu in Client/Server)
+
+# P2 will send a message saying it is interested in downloading the file
+
+# P1 has two options to respond to the message from P2,
+# self.choke(0) or self.un-choke (1)
+
+# uploader needs to call the file manager
+
+# uploading_bitfield is sending your bitfield to all the other peers in the network
+# downloading_bitfield used to download the file for P2 and putting it into persistent storage (blocks.txt)
+
+# Implementation:
+# 1. Listen for request from Peer 2 (peer trying to download file)
+#       a) interested (1)
+#       b) not interested (0)
+#
+# 2. If interested -> send response with
+#       a) un-choke (1) <permission to download>
+#       b) choke (0) <download not permitted>
+#    else -> continue listening
+#
+# 3. Now listen for next request from Peer 2
+#
+# 4. Forward request to download from Peer 2 to File Manager
+#       -> Request will contain a piece and a block
+#       -> send downloader bitfield
+#
+
+# Notes: Need to add code into the client.py so that uploader and client
+# of P2 can have this communication
